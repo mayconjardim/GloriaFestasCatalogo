56bb03673f90d536f8e56117c461b75093caad0e
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-11-25 22:29:01 -0300

    Entidade User e Navbar no client

---
+++ GloriaFestasCatalogo/Shared/Models/Users/User.cs
@@ -0,0 +1,13 @@
+﻿namespace GloriaFestasCatalogo.Shared.Models.Users
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateTime DataCreated { get; set; }
+
+    }
+}
