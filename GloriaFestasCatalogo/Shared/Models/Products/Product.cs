4bdb0f757af3cf82886c21a21346569719b14a52
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-11-25 23:07:11 -0300

    Entidade Product

---
+++ GloriaFestasCatalogo/Shared/Models/Products/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace GloriaFestasCatalogo.Shared.Models.Products
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PhotoUrl { get; set; }
+        public decimal Price { get; set; }
+        public ProductCategory Category { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
