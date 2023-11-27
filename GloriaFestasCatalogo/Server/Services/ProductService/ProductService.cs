45a69e58a1f6b9aec9ac13be54e3486b3c608b0c
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-11-27 09:40:37 -0300

    Metodos pra retorno do produto no Serviço

--- GloriaFestasCatalogo/Server/Services/ProductService/ProductService.cs
+++ GloriaFestasCatalogo/Server/Services/ProductService/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using GloriaFestasCatalogo.Server.Data;
+﻿using AutoMapper;
+using GloriaFestasCatalogo.Server.Data;
 using GloriaFestasCatalogo.Shared.Dtos.Products;
 using GloriaFestasCatalogo.Shared.Models.Products;
 using GloriaFestasCatalogo.Shared.Utils;
@@ -10,27 +11,85 @@ namespace GloriaFestasCatalogo.Server.Services.ProductService
     {
 
         private readonly DataContext _context;
-        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IMapper _mapper;
 
-        public ProductService(DataContext context, IHttpContextAccessor httpContextAccessor)
+        public ProductService(DataContext context, IHttpContextAccessor httpContextAccessor, IMapper mapper)
         {
             _context = context;
-            _httpContextAccessor = httpContextAccessor;
+            _mapper = mapper;
+
         }
 
-        public async Task<ServiceResponse<List<ProductDto>>> GetProductsByCategory(int categoryId)
+        public async Task<ServiceResponse<List<ProductDto>>> GetProductsAsync()
         {
+            var products = await _context.Products
+                .Include(p => p.Category)
+                .Where(p => p.Active)
+                .ToListAsync();
 
-            var response = new ServiceResponse<List<Product>>
+            var productDtos = _mapper.Map<List<ProductDto>>(products);
+
+            var response = new ServiceResponse<List<ProductDto>>
             {
-                Data = await _context.Products
-                    .Include(p => p.Category)
-                    .Where(p => p.Category.Id.Equals(categoryId) && p.Active)
-                    .ToListAsync()
+                Data = productDtos
             };
 
             return response;
         }
 
+        public async Task<ServiceResponse<ProductDto>> GetProductAsync(int productId)
+        {
+            var response = new ServiceResponse<ProductDto>();
+
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .Where(p => p.Id == productId && p.Active)
+                .FirstOrDefaultAsync();
+
+            if (product == null)
+            {
+                response.Success = false;
+                response.Message = "Desculpe, mas este produto não existe.";
+            }
+            else
+            {
+                var productDto = _mapper.Map<ProductDto>(product);
+                response.Data = productDto;
+            }
+
+            return response;
+        }
+
+        public async Task<ServiceResponse<List<ProductDto>>> GetProductsByCategory(int categoryId)
+        {
+            var products = await _context.Products
+                .Include(p => p.Category)
+                .Where(p => p.Category.Id.Equals(categoryId) && p.Active)
+                .ToListAsync();
+
+            var productDtos = _mapper.Map<List<ProductDto>>(products);
+
+            var response = new ServiceResponse<List<ProductDto>>
+            {
+                Data = productDtos
+            };
+
+            return response;
+        }
+
+        public Task<ServiceResponse<ProductDto>> CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<ProductDto>> UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<bool>> DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
