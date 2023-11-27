e98d14b90f8a7c3d911ca9972ab387765d3fa1fa
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-11-27 09:28:48 -0300

    Criando Product Service contrato e Implementação

---
+++ GloriaFestasCatalogo/Server/Services/ProductService/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using GloriaFestasCatalogo.Shared.Dtos.Products;
+using GloriaFestasCatalogo.Shared.Models.Products;
+using GloriaFestasCatalogo.Shared.Utils;
+
+namespace GloriaFestasCatalogo.Server.Services.ProductService
+{
+    public interface IProductService
+    {
+
+        Task<ServiceResponse<List<ProductDto>>> GetProductsAsync();
+        Task<ServiceResponse<ProductDto>> GetProductAsync(int productId);
+        Task<ServiceResponse<List<ProductDto>>> GetProductsByCategory(int categoryId);
+        Task<ServiceResponse<ProductDto>> CreateProduct(Product product);
+        Task<ServiceResponse<ProductDto>> UpdateProduct(Product product);
+        Task<ServiceResponse<bool>> DeleteProduct(int productId);
+
+    }
+}
---
+++ GloriaFestasCatalogo/Server/Services/ProductService/ProductService.cs
@@ -0,0 +1,34 @@
+﻿using GloriaFestasCatalogo.Server.Data;
+using GloriaFestasCatalogo.Shared.Models.Products;
+using GloriaFestasCatalogo.Shared.Utils;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloriaFestasCatalogo.Server.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+
+        private readonly DataContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ProductService(DataContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<ServiceResponse<List<Product>>> GetProductsByCategory(int categoryId)
+        {
+            var response = new ServiceResponse<List<Product>>
+            {
+                Data = await _context.Products
+                    .Include(p => p.Category)
+                    .Where(p => p.Category.Id.Equals(categoryId) && p.Active)
+                    .ToListAsync()
+            };
+
+            return response;
+        }
+
+    }
+}
