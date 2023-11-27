0be9b4abaaa7cf03fb511f4f50d596e204f3e5d9
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-11-27 13:35:08 -0300

    Criando CartService

---
+++ GloriaFestasCatalogo/Client/Services/CartService/CartService.cs
@@ -0,0 +1,87 @@
+﻿using Blazored.LocalStorage;
+using GloriaFestasCatalogo.Shared.Dtos.Products;
+using GloriaFestasCatalogo.Shared.Utils;
+
+
+namespace GloriaFestasCatalogo.Client.Services.CartService
+{
+    public class CartService
+    {
+
+        private readonly ILocalStorageService _localStorage;
+
+        public CartService(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+
+        }
+
+        public event EventHandler CartUpdated;
+
+        public async Task<List<CartProduct>> GetCart()
+        {
+            return await _localStorage.GetItemAsync<List<CartProduct>>("cart");
+        }
+
+        public async Task AddToCart(ProductDto product)
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartProduct>>("cart");
+
+            if (cart == null)
+            {
+                cart = new List<CartProduct>();
+            }
+
+            CartProduct existingCartItem = cart.FirstOrDefault(cp => cp.Product.Id == product.Id);
+
+            if (existingCartItem != null)
+            {
+                existingCartItem.Quantity++;
+            }
+            else
+            {
+                CartProduct cartProduct = new CartProduct
+                {
+                    Product = product,
+                    Quantity = 1
+                };
+
+                cart.Add(cartProduct);
+            }
+
+            await _localStorage.SetItemAsync("cart", cart);
+            CartUpdated?.Invoke(this, EventArgs.Empty);
+        }
+
+        public async Task UpdateCartItemQuantity(ProductDto product, int quantityChange)
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartProduct>>("cart");
+
+            if (cart == null)
+            {
+                return;
+            }
+
+            CartProduct existingCartItem = cart.FirstOrDefault(cp => cp.Product.Id == product.Id);
+
+            if (existingCartItem != null)
+            {
+                existingCartItem.Quantity += quantityChange;
+
+                if (existingCartItem.Quantity <= 0)
+                {
+                    cart.Remove(existingCartItem);
+                }
+
+                await _localStorage.SetItemAsync("cart", cart);
+                CartUpdated?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public async Task CleanCart()
+        {
+            await _localStorage.ClearAsync();
+        }
+
+    }
+}
---
+++ GloriaFestasCatalogo/Shared/Utils/CartProduct.cs
@@ -0,0 +1,12 @@
+﻿using GloriaFestasCatalogo.Shared.Dtos.Products;
+
+namespace GloriaFestasCatalogo.Shared.Utils
+{
+    public class CartProduct
+    {
+
+        public ProductDto Product { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
