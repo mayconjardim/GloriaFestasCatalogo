e9e1c778cb1fa703f1812e687c3647c5550cb386
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-11-27 10:07:42 -0300

    Criando ProductController

---
+++ GloriaFestasCatalogo/Server/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using GloriaFestasCatalogo.Server.Services.ProductService;
+using GloriaFestasCatalogo.Shared.Dtos.Products;
+using GloriaFestasCatalogo.Shared.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GloriaFestasCatalogo.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<ProductDto>>>> GetProducts()
+        {
+            var result = await _productService.GetProductsAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<ActionResult<ServiceResponse<ProductDto>>> GetProduct(int productId)
+        {
+            var result = await _productService.GetProductAsync(productId);
+            return Ok(result);
+        }
+
+        [HttpGet("category/{categoryId}")]
+        public async Task<ActionResult<ServiceResponse<List<ProductDto>>>> GetProductsByCategory(int categoryId)
+        {
+            var result = await _productService.GetProductsByCategory(categoryId);
+            return Ok(result);
+        }
+
+    }
+}
