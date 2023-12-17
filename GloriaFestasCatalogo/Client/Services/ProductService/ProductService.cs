f83c43c16fcb3ac5bb2f99bb7572cfee7c91fcf4
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-12-17 15:30:44 -0300

    Refaturando Product Service no Client

--- GloriaFestasCatalogo/Client/Services/ProductService/ProductService.cs
+++ GloriaFestasCatalogo/Client/Services/ProductService/ProductService.cs
@@ -16,102 +16,87 @@ namespace GloriaFestasCatalogo.Client.Services.ProductService
 
 		public async Task<ServiceResponse<List<ProductDto>>> GetProductsAsync()
 		{
-			var result = await _http.GetFromJsonAsync<ServiceResponse<List<ProductDto>>>($"api/product/all");
-			return result;
+			return await _http.GetFromJsonAsync<ServiceResponse<List<ProductDto>>>("api/product/all");
 		}
 
 		public async Task<ServiceResponse<ProductResponse>> GetProductsPageableAsync(int page, int pageSize, int categoryId, string? text = null)
 		{
-			var url = $"api/product/page/{page}?pageSize={pageSize}";
-
-			url += $"&categoryId={categoryId}";
+			var url = $"api/product/page/{page}?pageSize={pageSize}&categoryId={categoryId}";
 
 			if (!string.IsNullOrEmpty(text))
 			{
 				url += $"&text={Uri.EscapeDataString(text)}";
 			}
 
-			var result = await _http.GetFromJsonAsync<ServiceResponse<ProductResponse>>(url);
-			return result;
+			return await _http.GetFromJsonAsync<ServiceResponse<ProductResponse>>(url);
 		}
 
 		public async Task<ServiceResponse<ProductDto>> GetProductAsync(int productId)
 		{
-			var result = await _http.GetFromJsonAsync<ServiceResponse<ProductDto>>($"api/product/{productId}");
-			return result;
+			return await _http.GetFromJsonAsync<ServiceResponse<ProductDto>>($"api/product/{productId}");
 		}
 
 		public async Task<ServiceResponse<List<ProductDto>>> GetProductsByCategory(int categoryId)
 		{
-			var result = await _http.GetFromJsonAsync<ServiceResponse<List<ProductDto>>>($"api/product/category/{categoryId}");
-			return result;
+			return await _http.GetFromJsonAsync<ServiceResponse<List<ProductDto>>>($"api/product/category/{categoryId}");
 		}
 
 		public async Task<ServiceResponse<ProductDto>> CreateProduct(ProductCreateDto product)
 		{
 			var result = await _http.PostAsJsonAsync("api/product", product);
-
-			var newProduct = await result.Content.ReadFromJsonAsync<ServiceResponse<ProductDto>>();
-
-			return newProduct;
+			return await result.Content.ReadFromJsonAsync<ServiceResponse<ProductDto>>();
 		}
 
 		public async Task<ServiceResponse<bool>> DeleteProduct(int productId)
 		{
-			var response = new ServiceResponse<bool>();
-
 			try
 			{
 				var result = await _http.DeleteAsync($"api/product/{productId}");
-				response = await result.Content.ReadFromJsonAsync<ServiceResponse<bool>>();
+				return await result.Content.ReadFromJsonAsync<ServiceResponse<bool>>();
 			}
 			catch (Exception ex)
 			{
-				response.Success = false;
-				response.Message = ex.Message;
+				return new ServiceResponse<bool>
+				{
+					Success = false,
+					Message = ex.Message
+				};
 			}
-
-
-			return response;
 		}
 
 		public async Task<ServiceResponse<bool>> ActiveOrDeactiveProduct(ActiveOrDeactive activeOr)
 		{
-			var response = new ServiceResponse<bool>();
-
 			try
 			{
 				var result = await _http.PutAsJsonAsync($"api/product/active/{activeOr.ProductId}", activeOr);
-				response = await result.Content.ReadFromJsonAsync<ServiceResponse<bool>>();
+				return await result.Content.ReadFromJsonAsync<ServiceResponse<bool>>();
 			}
 			catch (Exception ex)
 			{
-				response.Success = false;
-				response.Message = ex.Message;
+				return new ServiceResponse<bool>
+				{
+					Success = false,
+					Message = ex.Message
+				};
 			}
-
-			return response;
 		}
 
 		public async Task<ServiceResponse<ProductDto>> UpdateProduct(ProductDto updatedProduct)
 		{
-			var response = new ServiceResponse<ProductDto>();
-
 			try
 			{
 				var result = await _http.PutAsJsonAsync($"api/product/{updatedProduct.Id}", updatedProduct);
-				response = await result.Content.ReadFromJsonAsync<ServiceResponse<ProductDto>>();
+				return await result.Content.ReadFromJsonAsync<ServiceResponse<ProductDto>>();
 			}
 			catch (Exception ex)
 			{
-				response.Success = false;
-				response.Message = ex.Message;
+				return new ServiceResponse<ProductDto>
+				{
+					Success = false,
+					Message = ex.Message
+				};
 			}
-
-			return response;
 		}
-
-
 
 	}
 }
