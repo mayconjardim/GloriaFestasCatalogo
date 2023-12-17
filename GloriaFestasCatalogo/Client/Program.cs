83024dc34c8b1ae49939dbb07baba90c559a8a48
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-12-17 16:06:24 -0300

    Instalando e configurando Blazor Boostrap

--- GloriaFestasCatalogo/Client/Program.cs
+++ GloriaFestasCatalogo/Client/Program.cs
@@ -11,28 +11,29 @@ global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace GloriaFestasCatalogo.Client
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
+			builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped<IProductService, ProductService>();
-            builder.Services.AddScoped<IOrderService, OrderService>();
-            builder.Services.AddScoped<ICategoryService, CategoryService>();
-            builder.Services.AddScoped<IAuthService, AuthService>();
-            builder.Services.AddScoped<CartService>();
+			builder.Services.AddScoped<IProductService, ProductService>();
+			builder.Services.AddScoped<IOrderService, OrderService>();
+			builder.Services.AddScoped<ICategoryService, CategoryService>();
+			builder.Services.AddScoped<IAuthService, AuthService>();
+			builder.Services.AddScoped<CartService>();
+			builder.Services.AddBlazorBootstrap();
 
-            builder.Services.AddOptions();
-            builder.Services.AddAuthorizationCore();
-            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
-            builder.Services.AddBlazoredLocalStorage();
+			builder.Services.AddOptions();
+			builder.Services.AddAuthorizationCore();
+			builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+			builder.Services.AddBlazoredLocalStorage();
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
-        }
-    }
+			await builder.Build().RunAsync();
+		}
+	}
 }
