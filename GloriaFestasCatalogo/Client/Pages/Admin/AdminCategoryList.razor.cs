944396956ab5e813177628aa6e9dd2d331103000
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-12-30 22:49:35 -0300

    Criando pagina AdminConfig no Client

--- GloriaFestasCatalogo/Client/Pages/Admin/AdminCategoryList.razor.cs
+++ GloriaFestasCatalogo/Client/Pages/Admin/AdminCategoryList.razor.cs
@@ -5,146 +5,146 @@ using Microsoft.JSInterop;
 
 namespace GloriaFestasCatalogo.Client.Pages.Admin
 {
-	partial class AdminCategoryList
-	{
+    partial class AdminCategoryList
+    {
 
-		private List<ProductCategoryDto> categories = new List<ProductCategoryDto>();
-		private ProductCategoryDto selectedCategorie;
-		private ProductCategoryDto newCategory = new ProductCategoryDto();
-		private string message = string.Empty;
-		private string searchText = string.Empty;
+        private List<ProductCategoryDto> categories = new List<ProductCategoryDto>();
+        private ProductCategoryDto selectedCategorie;
+        private ProductCategoryDto newCategory = new ProductCategoryDto();
+        private string message = string.Empty;
+        private string searchText = string.Empty;
 
-		[Inject] protected ToastService toastService { get; set; }
+        [Inject] protected ToastService toastService { get; set; }
 
-		protected override async Task OnInitializedAsync()
-		{
-			message = "Carregando Categorias...";
+        protected override async Task OnInitializedAsync()
+        {
+            message = "Carregando Categorias...";
 
-			var result = await CategoryService.GetCategoriesAsync(searchText);
-			if (!result.Success)
-			{
-				message = result.Message;
-			}
-			else
-			{
-				categories = result.Data;
-			}
-		}
+            var result = await CategoryService.GetCategoriesAsync(searchText);
+            if (!result.Success)
+            {
+                message = result.Message;
+            }
+            else
+            {
+                categories = result.Data;
+            }
+        }
 
-		private async Task FilterByText()
-		{
-			var result = await CategoryService.GetCategoriesAsync(searchText);
+        private async Task FilterByText()
+        {
+            var result = await CategoryService.GetCategoriesAsync(searchText);
 
-			if (!result.Success)
-			{
-				message = result.Message;
+            if (!result.Success)
+            {
+                message = result.Message;
 
-			}
-			else
-			{
-				categories = result.Data;
-			}
+            }
+            else
+            {
+                categories = result.Data;
+            }
 
-			await InvokeAsync(StateHasChanged);
-		}
+            await InvokeAsync(StateHasChanged);
+        }
 
-		private async Task OpenModal(string modal, int id)
-		{
+        private async Task OpenModal(string modal, int id)
+        {
 
-			var result = await CategoryService.GetCategorieAsync(id);
+            var result = await CategoryService.GetCategorieAsync(id);
 
-			if (!result.Success)
-			{
-				message = result.Message;
-			}
-			else
-			{
-				selectedCategorie = result.Data;
-			}
+            if (!result.Success)
+            {
+                message = result.Message;
+            }
+            else
+            {
+                selectedCategorie = result.Data;
+            }
 
-			await JSRuntime.InvokeAsync<object>("openModal", modal);
-		}
+            await JSRuntime.InvokeAsync<object>("openModal", modal);
+        }
 
-		private async Task CloseModal(string modal)
-		{
-			await JSRuntime.InvokeAsync<object>("closeModal", modal);
-		}
+        private async Task CloseModal(string modal)
+        {
+            await JSRuntime.InvokeAsync<object>("closeModal", modal);
+        }
 
-		private async Task CreateCategorie()
-		{
+        private async Task CreateCategorie()
+        {
 
-			if (newCategory.Name != null)
-			{
+            if (newCategory.Name != null)
+            {
 
-				var result = await CategoryService.CreateCategorie(newCategory);
-				if (result.Success)
-				{
-					await InvokeAsync(() =>
-					{
-						StateHasChanged();
-						toastService.Notify(new(ToastType.Success, $"Categoria criada com sucesso!"));
-					});
+                var result = await CategoryService.CreateCategorie(newCategory);
+                if (result.Success)
+                {
+                    await InvokeAsync(() =>
+                    {
+                        StateHasChanged();
+                        toastService.Notify(new(ToastType.Success, $"Categoria criada com sucesso!"));
+                    });
 
-					await CloseModal("CreateModal");
+                    await CloseModal("CreateModal");
 
-					RefreshPage();
-				}
-			}
+                    RefreshPage();
+                }
+            }
 
-		}
+        }
 
-		private async Task EditCategorie()
-		{
-			if (selectedCategorie != null)
-			{
-				var result = await CategoryService.UpdateCategorie(selectedCategorie);
-				if (result.Success)
-				{
-					await InvokeAsync(() =>
-					{
-						StateHasChanged();
-						toastService.Notify(new(ToastType.Success, $"Categoria editada com sucesso!"));
-					});
+        private async Task EditCategorie()
+        {
+            if (selectedCategorie != null)
+            {
+                var result = await CategoryService.UpdateCategorie(selectedCategorie);
+                if (result.Success)
+                {
+                    await InvokeAsync(() =>
+                    {
+                        StateHasChanged();
+                        toastService.Notify(new(ToastType.Success, $"Categoria editada com sucesso!"));
+                    });
 
-					await CloseModal("EditModal");
+                    await CloseModal("EditModal");
 
-					RefreshPage();
-				}
-			}
-		}
+                    RefreshPage();
+                }
+            }
+        }
 
-		private async Task DeleteCategorie()
-		{
-			if (selectedCategorie != null)
-			{
-				var result = await CategoryService.DeleteCategorie(selectedCategorie.Id);
-				if (result.Success)
-				{
-					await InvokeAsync(() =>
-					{
-						StateHasChanged();
-						toastService.Notify(new(ToastType.Success, $"Categoria deletada com sucesso!"));
-					});
+        private async Task DeleteCategorie()
+        {
+            if (selectedCategorie != null)
+            {
+                var result = await CategoryService.DeleteCategorie(selectedCategorie.Id);
+                if (result.Success)
+                {
+                    await InvokeAsync(() =>
+                    {
+                        StateHasChanged();
+                        toastService.Notify(new(ToastType.Success, $"Categoria deletada com sucesso!"));
+                    });
 
-					await CloseModal("DeleteModal");
+                    await CloseModal("DeleteModal");
 
-					RefreshPage();
+                    RefreshPage();
 
-				}
-			}
-		}
+                }
+            }
+        }
 
-		private async void UpdateCategoryNameSearch(ChangeEventArgs e)
-		{
-			searchText = e.Value.ToString();
-			await FilterByText();
-		}
+        private async void UpdateCategoryNameSearch(ChangeEventArgs e)
+        {
+            searchText = e.Value.ToString();
+            await FilterByText();
+        }
 
-		private async void RefreshPage()
-		{
-			await Task.Delay(1000);
-			NavigationManager.NavigateTo(NavigationManager.Uri, forceLoad: true);
-		}
+        private async void RefreshPage()
+        {
+            await Task.Delay(1000);
+            NavigationManager.NavigateTo(NavigationManager.Uri, forceLoad: true);
+        }
 
-	}
+    }
 }
