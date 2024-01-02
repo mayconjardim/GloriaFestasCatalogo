da652ad8e599fc73f0af40975c0a29d54883d4fa
Maycon Parreira Jardim <mayconsmill@gmail.com>
2024-01-02 20:24:30 -0300

    Criando Config Service contrato e implementação no Client

---
+++ GloriaFestasCatalogo/Client/Services/ConfigService/ConfigService.cs
@@ -0,0 +1,23 @@
+﻿using GloriaFestasCatalogo.Shared.Dtos.Config;
+using GloriaFestasCatalogo.Shared.Utils;
+using System.Net.Http.Json;
+
+namespace GloriaFestasCatalogo.Client.Services.ConfigService
+{
+	public class ConfigService
+	{
+
+		private readonly HttpClient _http;
+
+		public ConfigService(HttpClient http)
+		{
+			_http = http;
+		}
+
+		public async Task<ServiceResponse<AppConfigDto>> GetConfig()
+		{
+			return await _http.GetFromJsonAsync<ServiceResponse<AppConfigDto>>($"api/config/");
+		}
+
+	}
+}
---
+++ GloriaFestasCatalogo/Client/Services/ConfigService/IConfigService.cs
@@ -0,0 +1,12 @@
+﻿using GloriaFestasCatalogo.Shared.Dtos.Config;
+using GloriaFestasCatalogo.Shared.Utils;
+
+namespace GloriaFestasCatalogo.Client.Services.ConfigService
+{
+	public interface IConfigService
+	{
+
+		Task<ServiceResponse<AppConfigDto>> GetConfig();
+
+	}
+}
