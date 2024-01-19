7c6a2c118cfd14fd7befb20c7ddddd5d49248cbe
Maycon Parreira Jardim <mayconsmill@gmail.com>
2024-01-18 22:25:09 -0300

    Criando ProductVariant

---
+++ GloriaFestasCatalogo/Shared/Models/Products/ProductVariant.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GloriaFestasCatalogo.Shared.Models.Products
+{
+	public class ProductVariant
+	{
+
+		[JsonIgnore]
+		public Product Product { get; set; }
+		public int ProductId { get; set; }
+
+		public ProductType ProductType { get; set; }
+		public int ProductTypeId { get; set; }
+
+		public decimal Price { get; set; }
+
+	}
+}
