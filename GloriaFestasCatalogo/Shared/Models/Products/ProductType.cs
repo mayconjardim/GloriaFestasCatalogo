0c68ebb3e5f01dd85647062bbff0ce840d29ea8a
Maycon Parreira Jardim <mayconsmill@gmail.com>
2024-01-18 22:12:34 -0300

    Criando classe ProductType

---
+++ GloriaFestasCatalogo/Shared/Models/Products/ProductType.cs
@@ -0,0 +1,10 @@
+﻿namespace GloriaFestasCatalogo.Shared.Models.Products
+{
+	public class ProductType
+	{
+
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+
+	}
+}
