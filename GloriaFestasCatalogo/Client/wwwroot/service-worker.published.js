f3024fc7e55ea82e468d13e1b7621296b5c2cbd0
Maycon Jardim <mayconpjardim@gmail.com>
2024-04-11 14:14:40 -0300

    Removendo

--- GloriaFestasCatalogo/Client/wwwroot/service-worker.published.js
+++
@@ -1,6 +0,0 @@
-﻿// 📜 This is the "service-worker.published.js" file of your Blazor PWA.
-
-// 👇 Add these line to accept the message from this library.
-self.addEventListener('message', event => {
-    if (event.data?.type === 'SKIP_WAITING') self.skipWaiting();
-});
